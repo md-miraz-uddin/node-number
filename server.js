5047f3e91892d6965b569f5bff842d80ba7847f7
Miraz_Uddin <mirazuddin0623@gmail.com>
2021-10-01 11:56:25 +0600

    Calculation Done

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log(process.argv);
+let x = parseInt(process.argv[2]);
+let y = parseInt(process.argv[3]);
+let summation = x+y;
+let multiplication = x*y;
+let subtraction = x-y;
+let dividation = x-y;
+let array = [ summation, multiplication, subtraction, dividation ];
+console.log(array);
